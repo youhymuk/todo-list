0712a4ad03dd7a4accb04d5158e8737fbc8d19f0
Dima <dima.yukhimuk@gmail.com>
2020-05-28 09:46:48 +0300

    Updates

---
+++ precache-manifest.4077568a0dd5a4c3f0309027bcdf3ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed40c3a93b5b7f751de2c4c15f4ee5c2",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "0215eed117db7b68da69",
+    "url": "/todo-list/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "bdb3ddc4dfe93f6d84ad",
+    "url": "/todo-list/static/css/main.91d6b987.chunk.css"
+  },
+  {
+    "revision": "0215eed117db7b68da69",
+    "url": "/todo-list/static/js/2.3aaa35c6.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/todo-list/static/js/2.3aaa35c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb3ddc4dfe93f6d84ad",
+    "url": "/todo-list/static/js/main.af34138a.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
